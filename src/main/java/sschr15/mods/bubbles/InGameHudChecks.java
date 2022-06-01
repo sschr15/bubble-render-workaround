7d1bbad08610074d2459ee81c1c29a440e53dbdc
sschr15 <steven.sschr15@hotmail.com>
2022-06-01 14:56:56 -0500

    Initial commit

---
+++ src/main/java/sschr15/mods/bubbles/InGameHudChecks.java
@@ -0,0 +1,40 @@
+package sschr15.mods.bubbles;
+
+import io.github.apace100.origins.power.OriginsPowerTypes;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.tag.TagKey;
+
+public class InGameHudChecks {
+    public static boolean showBubbles(PlayerEntity player, TagKey<Fluid> tagKey) {
+
+        //region: Water breathing effect
+        if (player.hasStatusEffect(StatusEffects.WATER_BREATHING)) {
+            return false; // Never show air bar if the player has water breathing
+        }
+        //endregion
+
+        //region: Origins
+        try {
+            Class.forName("io.github.apace100.origins.power.OriginsPowerTypes");
+            // If the class exists, Origins is installed, meaning we need to do more checks!
+
+            if (OriginsPowerTypes.WATER_BREATHING.isActive(player)) {
+                return !player.isSubmergedIn(tagKey); // Invert the normal check
+            }
+        } catch (ClassNotFoundException e) {
+            // Origins is not installed, so we can continue trying other stuff
+        }
+        //endregion
+
+        //region: Any generic "gills" effect
+        if (player.getStatusEffects().stream().anyMatch(effect -> effect.getEffectType().getTranslationKey().contains(".gills"))) {
+            return !player.isSubmergedIn(tagKey); // Invert the normal check
+        }
+        //endregion
+
+        // Default to the normal check
+        return player.isSubmergedIn(tagKey);
+    }
+}
---
+++ src/main/java/sschr15/mods/bubbles/mixins/InGameHudMixin.java
@@ -0,0 +1,18 @@
+package sschr15.mods.bubbles.mixins;
+
+import sschr15.mods.bubbles.InGameHudChecks;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.tag.TagKey;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+    @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isSubmergedIn(Lnet/minecraft/tag/TagKey;)Z"))
+    private boolean isSubmergedIn(PlayerEntity entity, TagKey<Fluid> tagKey) {
+        return InGameHudChecks.showBubbles(entity, tagKey);
+    }
+}
